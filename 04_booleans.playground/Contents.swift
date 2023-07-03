fc60ade96270663ecd425b4b1821747778f9d3dc
Miguel <migueljomartinez@gmail.com>
2023-07-03 14:21:41 -0500

    Working with tuples

--- 04_booleans.playground/Contents.swift
+++ 04_booleans.playground/Contents.swift
@@ -10,3 +10,17 @@ if isDeliciousFood {
 } else {
     print("La comida no está deliciosa")
 }
+
+// Tuplas
+let http404Error = (404, "Not found")
+let (statusCode, statusMessage) = http404Error
+
+print("Status code: \(statusCode); Status message: \(statusMessage)")
+
+let (justStatusCode, _) = http404Error
+print(justStatusCode)
+
+print("Status code: \(http404Error.0) - Status message: \(http404Error.1)")
+
+let http200Status = (statusCode: 200, statusMessage: "Ok")
+print("Status code: \(http200Status.statusCode) - Status message: \(http200Status.statusMessage)")
