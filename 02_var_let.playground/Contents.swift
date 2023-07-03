b6170ef49d0cf64bbcace36ba2cdb1c9fa0274a1
Miguel <migueljomartinez@gmail.com>
2023-07-02 22:46:18 -0500

    WIP: Numbers

--- 02_var_let.playground/Contents.swift
+++ 02_var_let.playground/Contents.swift
@@ -6,3 +6,29 @@ print(name)
 
 let age = 28
 print(age)
+
+var x = 0.0, y = 0.0
+
+var welcomeMessage: String
+
+welcomeMessage = "Hola, que tal?"
+
+var red, green, blue: Double
+
+let π = 4.14912
+let Ω = 0
+
+let 😁 = "Smile 😁"
+print(😁)
+
+print("Interpolation \(π) - \(x) - \(y)" + " Sufijo")
+
+let array = [1, 2, 3, 4, 5, 6]
+
+print(array, separator: " - ", terminator: " Done")
+
+// This is a comment
+
+/**
+ This is a multi line comment
+ */
