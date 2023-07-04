e516407970d1eb5b71aeb62de8d216cb24ecbdaa
Miguel <migueljomartinez@gmail.com>
2023-07-03 22:33:24 -0500

    Optionals

--- 04_booleans.playground/Contents.swift
+++ 04_booleans.playground/Contents.swift
@@ -25,3 +25,19 @@ print("Status code: \(http404Error.0) - Status message: \(http404Error.1)")
 let http200Status = (statusCode: 200, statusMessage: "Ok")
 print("Status code: \(http200Status.statusCode) - Status message: \(http200Status.statusMessage)")
 
+// Optionals
+let ageString = "28"
+let convertedAge = Int(ageString)
+
+print(type(of: convertedAge)) // Optional(Int)
+
+var serverResponseCode: Int? = 404
+serverResponseCode = nil
+
+var serveyAnswer: String?
+
+if convertedAge != nil {
+    print("Age exists: \(convertedAge!)")
+} else {
+    print("Age does not exist")
+}
