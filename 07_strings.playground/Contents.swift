9bbac9fd91937fa7400dbc0583952c129c6ed800
Miguel <migueljomartinez@gmail.com>
2023-07-06 23:24:43 -0500

    Playing with strings

--- 07_strings.playground/Contents.swift
+++ 07_strings.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let someString = "Cadena de texto"
+let multilineString = """
+    Multi line text
+    One
+    Two Three
+"""
+
+print(multilineString)
+
+let wiseWords = "\"La imaginación es mas importante que el saber\""
+print(wiseWords)
+
+let dolarSign = "\u{24} $ \u{2666} \u{1f496} 😁 🇨🇴 \u{1F1E8}\u{1F1F4}"
