f581f231181fc730c783c281dc22403583084369
Miguel <migueljomartinez@gmail.com>
2023-07-07 00:54:57 -0500

    Playing with strings

--- 07_strings.playground/Contents.swift
+++ 07_strings.playground/Contents.swift
@@ -13,3 +13,30 @@ let wiseWords = "\"La imaginación es mas importante que el saber\""
 print(wiseWords)
 
 let dolarSign = "\u{24} $ \u{2666} \u{1f496} 😁 🇨🇴 \u{1F1E8}\u{1F1F4}"
+
+var emptyString = ""
+var anotherEmptyString = String()
+
+if emptyString.isEmpty {
+    print("Empty string is indeed empty")
+}
+
+var newSomeString = "Un caballo"
+newSomeString += " y un carruaje"
+newSomeString += " y un soldado"
+
+let name = "Juan Gabriel 😎"
+
+for char in name {
+    print("Current char: \(char)")
+}
+
+let exclamationMark: Character = "!"
+let nameChars: [Character] = ["J", "o", "n"]
+var nameString = String(nameChars + "y")
+
+nameString.append(exclamationMark)
+"\(nameString) \(exclamationMark)"
+
+let multiplier = 3
+let message = "El producto de \(multiplier) x 3.5 es \(Double(multiplier) * 3.5)\(exclamationMark)"
