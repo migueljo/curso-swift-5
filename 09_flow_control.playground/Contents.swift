6cf848dd43cd5f1a6f5fcd470ad2b806e54a1a40
Miguel <migueljomartinez@gmail.com>
2023-07-09 15:13:13 -0500

    Calculating prime numbers between 1 and 100

--- 09_flow_control.playground/Contents.swift
+++ 09_flow_control.playground/Contents.swift
@@ -30,3 +30,43 @@ let minutes = 60
 for tickMark in stride(from: 0, to: minutes, by: 5) {
     print("\(hour):\(tickMark)")
 }
+
+// While
+let limit = 10
+var i = 0
+while i < limit {
+    print(i)
+    i += 1
+}
+
+repeat {
+    print("Hello from repeat")
+} while i > 100
+
+// Calculate prime numbers between 0 and 100
+func isPrime(number: Int) -> Bool {
+    var i = 1
+    var counter = 0
+    while i <= number {
+        if number % i == 0 {
+            counter += 1
+        }
+        if counter > 2 {
+            return false
+        }
+        i += 1
+    }
+    return counter == 2
+}
+
+var primeNumbers = [Int]()
+var primeNumbersLimit = 100
+var primeNumberIndex = 1
+
+while primeNumberIndex <= primeNumbersLimit {
+    if isPrime(number: primeNumberIndex) {
+        primeNumbers += [primeNumberIndex]
+    }
+    primeNumberIndex += 1
+}
+print(primeNumbers)
