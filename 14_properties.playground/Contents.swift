1469820518b3823cbc9110afb75917b2add8931b
Miguel <migueljomartinez@gmail.com>
2023-07-14 23:47:56 -0500

    Lazy properties

--- 14_properties.playground/Contents.swift
+++ 14_properties.playground/Contents.swift
@@ -12,3 +12,22 @@ rangeOfThreeItems.firstValue = 6
 let fourItemsRange = FixedLengthRange(firstValue: 0, length: 4)
 // fourItemsRange.firstValue = 3 This throws error
 
+// Lazy stored properties
+
+class DataImported {
+    var filename = "data.txt"
+}
+
+class DataManager {
+    lazy var importer = DataImported()
+    var data = [String]()
+}
+
+let manager = DataManager()
+manager.data.append("Datos")
+manager.data.append("Más datos")
+manager
+// Hasta esta linea el importer no ha sido creado debido a que es una lazy property
+manager.importer.filename
+// Aqui ya está creada
+manager
