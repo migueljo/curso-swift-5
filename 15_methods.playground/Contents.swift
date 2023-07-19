d28d185e37bba3865c14ec2c26c16b2780f8f570
Miguel <migueljomartinez@gmail.com>
2023-07-18 22:56:22 -0500

    Working with mutating methods in Struct and Enum

--- 15_methods.playground/Contents.swift
+++ 15_methods.playground/Contents.swift
@@ -35,12 +35,43 @@ counter.count
 counter.reset()
 counter.count
 
+// Mutating methods in Struct and Enum
+
 struct Point {
     var x = 0.0, y = 0.0
     func isToTheRight(of x: Double) -> Bool {
         return self.x > x
     }
+    
+    mutating func moveBy(x deltaX: Double, y deltaY: Double) {
+        // self.x += deltaX
+        // self.y += deltaY
+        self = Point(x: self.x + deltaX, y: self.y + deltaY)
+    }
 }
 
-let point = Point(x: 4, y: 5)
+var point = Point()
+point.x = 4
+point.y = 5
 point.isToTheRight(of: 3)
+point.moveBy(x: 10, y: 10)
+
+enum DifferentStateSwitch {
+    case off, low, high
+    mutating func next() {
+        switch self {
+            case .off:
+                self = .low
+            case .low:
+                self = .high
+            case .high:
+                self = .off
+        }
+        
+    }
+}
+
+var switchState = DifferentStateSwitch.off
+switchState.next()
+switchState.next()
+switchState.next()
