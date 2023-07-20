3b2a48150c4ea157a5dbbdb7a338f44c6d1c14c9
Miguel <migueljomartinez@gmail.com>
2023-07-20 14:50:36 -0500

    Initializers part 2

--- 18_initializers.playground/Contents.swift
+++ 18_initializers.playground/Contents.swift
@@ -25,3 +25,37 @@ let waterBoilingPoint = Celcius(fromFahrenheit: 212)
 let waterFreezingPoint = Celcius(fromKelvin: 273.15)
 waterBoilingPoint.temperature
 waterFreezingPoint.temperature
+
+struct Color {
+    let red, green, blue: Double
+    init(red: Double, green: Double, blue: Double) {
+        self.red = red
+        self.green = green
+        self.blue = blue
+    }
+    init(white: Double) {
+        self.red = white
+        self.green = white
+        self.blue = white
+    }
+}
+
+let magenta = Color(red: 1, green: 0, blue: 1)
+let halfGray = Color(white: 0.5)
+let gree = Color(red: 0, green: 1, blue: 0)
+
+class SurveyQuestion {
+    var text: String
+    var response: String?
+    
+    init(text: String) {
+        self.text = text
+    }
+    
+    func ask() {
+        print(self.text)
+    }
+}
+
+let q1 = SurveyQuestion(text: "Te gustan los tacos?")
+q1.ask()
