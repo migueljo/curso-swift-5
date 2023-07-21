3ac2d333c3e11f862c25ae0eaada447b477b630e
Miguel <migueljomartinez@gmail.com>
2023-07-20 20:43:14 -0500

    Optional chaining part 3

--- 19_optional_chaining.playground/Contents.swift
+++ 19_optional_chaining.playground/Contents.swift
@@ -1,37 +1,11 @@
 import UIKit
 
-/**
- class Person {
-     var residence: Residence?
- }
-
- class Residence {
-     var numberOfRooms = 1
- }
-
- let edgar = Person()
- let roomCount = edgar.residence?.numberOfRooms
-
- if let roomCount2 = edgar.residence?.numberOfRooms {
-     print("La casa de Edgar tiene \(roomCount2) habitación(es)")
- } else {
-     print("Edgar no tiene casa 😢")
- }
-
- edgar.residence = Residence()
- if let roomCount2 = edgar.residence?.numberOfRooms {
-     print("La casa de Edgar tiene \(roomCount2) habitación(es)")
- } else {
-     print("Edgar no tiene casa 😢")
- }
-
- */
-
 class Person {
     var residence: Residence?
 }
 
 class Residence {
+    var address: Address?
     var rooms = [Room]()
     var numberOfRooms: Int {
         return self.rooms.count
@@ -43,6 +17,9 @@ class Residence {
         set {
             self.rooms[i] = newValue
         }
+    }
+    func printNumberOfRoom() {
+        print("El número de habitaciones es \(numberOfRooms)")
     }
 }
 
@@ -67,4 +44,57 @@ class Address {
             return nil
         }
     }
+}
+
+func createAddress() -> Address {
+    print("Creating address")
+    let someAddress = Address()
+    someAddress.buildingNumber = "13"
+    someAddress.street = "Calle de Platzi"
+    return someAddress
+}
+
+let edgar = Person()
+if let roomCount = edgar.residence?.numberOfRooms {
+    print("La casa de Edgar tiene \(roomCount) habitación(es)")
+} else {
+    print("Edgar no tiene casa 😢")
+}
+edgar.residence?.address = createAddress()
+edgar.residence?.printNumberOfRoom()
+
+func x() {
+    print("X")
+}
+
+if edgar.residence?.printNumberOfRoom() != nil {
+    print("He podido obtener el número de habitaciones")
+} else {
+   print("No ha sido posible saber el número de habitaciones")
+}
+
+if (edgar.residence?.address = createAddress()) != nil {
+    print("Dirección de Edgar configurada")
+} else {
+    print("Seguimos sin saber donde vive Edgar")
+}
+
+if let firstRoomName = edgar.residence?[0].name {
+    print("Primera habitación: \(firstRoomName)")
+} else {
+    print("No hay primera habitación")
+}
+
+edgar.residence?[0] = Room(name: "Bathroom")
+
+let edgarHouse = Residence()
+edgarHouse.rooms.append(Room(name: "Bathroom"))
+edgarHouse.rooms.append(Room(name: "Living room"))
+edgarHouse.rooms.append(Room(name: "Kitchen"))
+edgar.residence = edgarHouse
+
+if let firstRoomName = edgar.residence?[0].name {
+    print("Primera habitación: \(firstRoomName)")
+} else {
+    print("No hay primera habitación")
 }
